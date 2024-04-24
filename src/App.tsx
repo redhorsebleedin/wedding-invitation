3b0d3fa3d9b4f41fe5398ef5d5177d3b7285ecac
Muhamad Rizal <redhorse@Muhamads-MacBook-Air.local>
2024-04-24 10:07:58 +0700

    add animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,209 +1,185 @@
-import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
-import { animated, useSpring, useTransition } from "@react-spring/web";
-import classNames from "classnames";
-import { useEffect, useRef } from "react";
-import { BiChevronDown, BiChevronUp } from "react-icons/bi";
+import { motion, useScroll, useSpring } from "framer-motion";
 import "./App.css";
+import Layer from "./Layer";
+import AnimatedText from "./components/AnimatedText";
 
-function App() {
-  const url = (name: string, wrap = false) =>
-    `${
-      wrap ? "url(" : ""
-    }https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
-      wrap ? ")" : ""
-    }`;
-  const parallax = useRef<IParallax>(null!);
-
-  // LAYER 1
-
-  const welcomeAnimation = useTransition(
-    [
-      { id: 1, text: "Hi," },
-      { id: 2, text: "Rizal" },
-      { id: 3, text: "You're invited!" },
-    ],
-    {
-      from: { opacity: 0, y: 30 },
-      enter: { opacity: 1, y: 0 },
-      leave: { opacity: 1, y: 0 },
-      trail: 100,
-    }
-  );
-
-  const welcomeWrapper = useSpring({
-    from: { opacity: 0, y: -30 },
-    to: { opacity: 1, y: 0 },
-  });
-
-  // END LAYER 1
-
-  // LAYER 2
-
-  const [layer2Text, layer2TextApi] = useTransition(
-    [
-      { id: 1, text: "THE WEDDING OF" },
-      { id: 2, text: "RIZAL & HANIFA" },
-    ],
-    () => ({
-      from: {
-        x: 100,
-        y: 250,
-        scale: 1.5,
-      },
-      enter: {
-        x: 0,
-        y: 0,
-        scale: 1,
-      },
-      leave: {
-        x: 0,
-        y: 0,
-        scale: 3,
-      },
-      delay: 1500,
-      trail: 400,
-    })
-  );
-
-  const [layer2Bubble, layer2BubbleApi] = useSpring(() => ({
-    from: {
-      scale: 0,
-      borderRadius: "50%",
-    },
-  }));
-
-  // END LAYER 2
-
-  const handleScroll = () => {
-    console.log(parallax.current.offset);
-    if (parallax?.current?.offset === 1) {
-      layer2TextApi.start();
-      layer2BubbleApi.start({
-        to: {
-          scale: 1,
-          borderRadius: "0",
-        },
-        delay: 3000,
-      });
-    }
-  };
-
-  useEffect(() => {
-    const container = document.querySelector(".my-class-name");
-    container?.addEventListener("scroll", handleScroll);
-    return () => {
-      container?.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+export default function App() {
+  // const { scrollYProgress } = useScroll();
+  // const scaleX = useSpring(scrollYProgress, {
+  //   stiffness: 100,
+  //   damping: 30,
+  //   restDelta: 0.001,
+  // });
 
   return (
-    <div className="w-screen h-fit- bg-[#253237] !overflow-hidden">
-      <div className="fixed bottom-4 right-4 text-white flex flex-col gap-2 z-50">
-        <button
-          className="rounded-full shadow-md items-center justify-center bg-[rgba(0,0,0,0.2)]"
-          onClick={() => {
-            if (parallax.current?.offset > 0) {
-              parallax.current?.scrollTo(parallax.current.offset - 1);
-            }
-          }}
+    <div className="w-screen">
+      <div className="w-full h-[90vh] relative flex flex-col justify-end overflow-hidden font-oceanwide">
+        <Layer speed={100} className="absolute w-full h-full top-0">
+          <div className="w-full h-1/2 bg-[#020E56]"></div>
+          <div className="w-full h-1/2 bg-white"></div>
+        </Layer>
+        <Layer speed={100} className="absolute w-full">
+          <img src="/parallax-bg.svg" className="w-full" />
+        </Layer>
+        {/* <Layer speed={1700} className="absolute w-full top-0 rotate-180">
+          <img src="/stars.svg" className="w-full" />
+        </Layer> */}
+        <Layer speed={1400} className="absolute w-full">
+          <img src="/stars.svg" className="w-full" />
+        </Layer>
+        <Layer speed={1300} className="absolute w-full">
+          <img src="/cloud-5.svg" className="w-full" />
+        </Layer>
+        <Layer speed={1000} className="absolute w-full">
+          <img src="/cloud-4.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={800}
+          className="absolute w-full"
+          initial={{ x: -30, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
         >
-          <BiChevronUp size={48} />
-        </button>
-        <button
-          className="rounded-full shadow-md items-center justify-center bg-[rgba(0,0,0,0.2)]"
-          onClick={() => {
-            parallax.current?.scrollTo(parallax.current.offset + 1);
-          }}
+          <img src="/cloud-3.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={1100}
+          className="absolute w-full"
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
         >
-          <BiChevronDown size={48} />
-        </button>
+          <img src="/cloud-2.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={1200}
+          className="absolute w-full"
+          initial={{ x: 30, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+        >
+          <img src="/cloud-1.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={800}
+          className="absolute w-full"
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-7.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={700}
+          className="absolute w-full"
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-6.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={600}
+          className="absolute w-full"
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-5.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={500}
+          className="absolute w-full"
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-4.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={400}
+          className="absolute w-full"
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-3.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={300}
+          className="absolute w-full"
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-2.svg" className="w-full" />
+        </Layer>
+        <Layer
+          speed={800}
+          className="absolute w-full h-full text-white flex flex-col items-center pt-48"
+        >
+          <AnimatedText text="Hi RUSLI" className="text-3xl" />
+          <AnimatedText
+            text="You're invited!"
+            className="text-3xl "
+            delay={0.5}
+          />
+        </Layer>
+        <Layer
+          speed={100}
+          className="absolute w-full"
+          initial={{ y: 30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+        >
+          <img src="/mosque-1.svg" className="w-full" />
+        </Layer>
+        <Layer
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          speed={0}
+          className="absolute w-full"
+        >
+          <img src="/couple-1.png" className="w-full" />
+        </Layer>
       </div>
-
-      <Parallax
-        ref={parallax}
-        pages={3}
-        className="!overflow-hidden my-class-name"
-        // className="my-class-name"
-      >
-        {/* LAYER 1 */}
-        <ParallaxLayer offset={0} speed={0.1}>
-          <div className="bg-[url(https://www.kalacerita.com/wp-content/uploads/2024/02/GITAFIKRI-123.jpg)] h-screen w-screen bg-cover bg-center"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.67} speed={0.05}>
-          <animated.img src="/shape2.png" style={welcomeWrapper} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.7} speed={0}>
-          <animated.img src="/shape1.png" style={welcomeWrapper} />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.77} speed={-0.05}>
-          <animated.div className="self-center justify-center p-8">
-            {welcomeAnimation((style, item) => {
-              console.log(style, item);
-              return (
-                <animated.p
-                  className={classNames(
-                    (item.id === 1 || item.id === 3) &&
-                      "text-[#EC014F] text-[25px] -my-5",
-                    item.id === 2 && "text-[#5200CE] text-[50px] -ml-1"
-                  )}
-                  style={style}
-                >
-                  {item.text}
-                </animated.p>
-              );
-            })}
-          </animated.div>
-        </ParallaxLayer>
-        {/* END LAYER 1 */}
-
-        {/* LAYER 2 */}
-        <ParallaxLayer
-          offset={1}
-          speed={0}
-          style={{ backgroundColor: "#24DBD7" }}
-          className="p-4 leading-tight"
-        >
-          {layer2Text((style, item) => {
-            console.log(style, item);
-            return (
-              <animated.p
-                className={
-                  item.id === 1
-                    ? "text-[#EC014F] text-[15px]"
-                    : "text-[#5200CE] text-[25px]"
-                }
-                style={style}
-              >
-                {item.text}
-              </animated.p>
-            );
-          })}
-        </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0}>
-          <animated.div
-            className="w-full h-full bg-[#FEE50F]"
-            style={layer2Bubble}
-          ></animated.div>
-        </ParallaxLayer>
-        {/* END LAYER 2 */}
-        <ParallaxLayer
-          offset={2}
-          speed={0}
-          style={{ backgroundColor: "#e54848" }}
-        />
-
-        <ParallaxLayer
-          offset={0}
-          speed={-0.5}
-          factor={4}
-          style={{
-            backgroundImage: url("stars", true),
-            backgroundSize: "cover",
-          }}
-        />
-      </Parallax>
+      <div className="w-full rounded-xl h-[2000px] bg-white">
+        <div className="w-full h-screen relative">
+          <Layer
+            speed={0}
+            className="w-full text-[#29296D] text-3xl flex flex-col p-4 sticky top-0"
+          >
+            <motion.span
+              initial={{
+                y: 50,
+                opacity: 0,
+              }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+              }}
+              viewport={{
+                amount: 1,
+                once: true,
+                margin: "0px 0px -300px 0px",
+              }}
+              transition={{ ease: "linear" }}
+              className="overflow-hidden"
+            >
+              To the wedding of
+            </motion.span>
+            <motion.span
+              initial={{
+                y: 50,
+                opacity: 0,
+              }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+              }}
+              viewport={{
+                amount: 1,
+                once: true,
+                margin: "0px 0px -300px 0px",
+              }}
+              transition={{ ease: "linear", delay: 0.1 }}
+            >
+              Rizal & Hanifa
+            </motion.span>
+          </Layer>
+        </div>
+      </div>
+      <div className="h-[2000px]"></div>
     </div>
   );
 }
-
-export default App;
